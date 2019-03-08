837f6c96e1e19a6c71628f81e94af0772b3ba0a2
Tim Otten <totten@civicrm.org>
2019-03-07 23:34:24 -0800

    Reconcile both description and license

---
+++ scriptlet/reconcile/110-basic-i2c.php
@@ -0,0 +1,24 @@
+<?php
+use \Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Map some simple 1-1 fields from "info.xml" to "composer.json" -- "description", "license"
+ */
+return function(OutputInterface $output, SimpleXMLElement $infoXml, &$composerJson) {
+  $map = [
+    'description' => 'description',
+    'license' => 'license',
+  ];
+  foreach ($map as $iField => $cField) {
+    $value = (string) $infoXml->{$iField};
+    if (empty($value)) {
+      $value = '';
+    }
+
+    if (!isset($composerJson[$cField])) {
+      $output->writeln("<info>In <comment>composer.json</comment>, add <comment>$cField</comment>.</info>", OutputInterface::VERBOSITY_VERBOSE);
+      $composerJson[$cField] = $value;
+    }
+  }
+
+};
