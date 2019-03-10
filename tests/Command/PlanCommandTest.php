2e6918f371ee7619e8b547f69ccc740905e6c4e8
Tim Otten <totten@civicrm.org>
2019-03-09 16:42:53 -0800

    PlanCommandTest - Improve debugging

--- tests/Command/PlanCommandTest.php
+++ tests/Command/PlanCommandTest.php
@@ -21,14 +21,14 @@ class PlanCommandTest extends \Comex\ComexTestCase {
 
     $linePatterns = [
       ';' . preg_quote('comex build --ext=\'org.civicrm.api4\' --git-url=\'https://github.com/civicrm/org.civicrm.api4\' --commit=\'be447c73fb6a31f2535869500f64054131305da7\' --ver=\'4.0.0\' --web-root=\'/tmp/myweb/\'', ';') . ';',
-      ';' . preg_quote('comex build --ext=\'org.civicrm.api4\' --git-url=\'https://github.com/civicrm/org.civicrm.api4\' --commit=\'d5a853a6f4d1cad11e8655755b329f15eb3fc27b\' --ver=\'4.1.0\' --web-root=\'/tmp/myweb/\'') . ';'
+      ';' . preg_quote('comex build --ext=\'org.civicrm.api4\' --git-url=\'https://github.com/civicrm/org.civicrm.api4\' --commit=\'d5a853a6f4d1cad11e8655755b329f15eb3fc27b\' --ver=\'4.1.0\' --web-root=\'/tmp/myweb/\'', ';') . ';'
     ];
 
     $allOutput = $commandTester->getDisplay(FALSE);
     $lines = explode("\n", $allOutput);
     foreach ($linePatterns as $linePattern) {
       $grep = preg_grep($linePattern, $lines);
-      $this->assertCount(1, $grep);
+      $this->assertCount(1, $grep, "Expected one match for pattern ($linePattern)");
     }
   }
 
